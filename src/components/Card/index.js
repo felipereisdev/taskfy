de5a67db02df28a2b501ee0ec43382eb1347e88d
Felipe Reis <felipe.reis@pdmfc.com>
2020-03-19 17:42:34 +0000

    first commit wip

---
+++ src/components/Card/index.js
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+import { Container, Label } from './styles';
+
+export default function Card({ data, index }) {
+  const ref = useRef();
+
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'CARD', index, id: data.id, content: data.content },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) {
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if (draggedIndex === targetIndex) {
+        return;
+      }
+    },
+  });
+
+  dragRef(dropRef(ref));
+
+  return (
+    <Container ref={ref} isDragging={isDragging}>
+      <header>
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
+      </header>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt="avatar" />}
+    </Container>
+  );
+}
